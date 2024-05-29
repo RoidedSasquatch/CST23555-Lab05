8bfd178649d5953a18ffd4b899e785e04d9408e0
Dan Blais <blai0327@algonquinlive.com>
2024-05-29 11:41:37 -0400

    Second Commit: Implemented EncryptedSharedPrefs, an alert message popup and snackbar.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:encrypted_shared_preferences/encrypted_shared_preferences.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -59,6 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
   late TextEditingController passController;
   var passText = "";
   var source = "assets/question-mark.png";
+  EncryptedSharedPreferences prefs = EncryptedSharedPreferences();
 
   void buttonClick() {
     setState(() {
@@ -68,7 +70,37 @@ class _MyHomePageState extends State<MyHomePage> {
       } else {
         source = "assets/stop.png";
       }
+      showDialog<String>(
+        context: context,
+        builder: (BuildContext context) =>  AlertDialog(
+          title: const Text('Save Login Details?'),
+          content: const Text('Would you like to save your username and password for next time?'),
+          actions: <Widget>[
+            ElevatedButton(onPressed: okClicked, child: const Text("Ok")),
+            ElevatedButton(onPressed: cancelClicked, child: const Text("Cancel"))
+          ],
+        ),
+      );
     });
+  }
+
+  void okClicked() async {
+      prefs.setString("username", loginController.value.text);
+      prefs.setString("password", passController.value.text);
+      Navigator.pop(context);
+  }
+
+  void cancelClicked() async {
+      prefs.remove("username");
+      prefs.remove("password");
+      Navigator.pop(context);
+  }
+
+  void clearData() async {
+    loginController.text = "";
+    passController.text = "";
+    prefs.remove("username");
+    prefs.remove("password");
   }
 
   @override
@@ -76,6 +108,22 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
     loginController = TextEditingController();
     passController = TextEditingController();
+    prefs.getString("username").then((String value) {
+      if(value != "") {
+        loginController.text = value;
+        SnackBar snackBar = SnackBar(
+            content: Text("Welcome $value, previous login details loaded."),
+            action: SnackBarAction(label: "Clear Data", onPressed: clearData));
+        Future.delayed(const Duration(seconds: 1)).then((_) {
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        });
+      }
+    });
+    prefs.getString("password").then((String value) {
+      if(value != "") {
+        passController.text = value;
+      }
+    });
   }
 
   @override
@@ -129,13 +177,14 @@ class _MyHomePageState extends State<MyHomePage> {
                     labelText: "Login"
                 )),
             TextField(controller: passController,
+                obscureText: true,
                 decoration: const InputDecoration(
                     hintText:"Enter Password",
                     border: OutlineInputBorder(),
                     labelText: "Password"
                 )),
             ElevatedButton(onPressed: buttonClick, child: const Text("Login")),
-            Image.asset(source),
+            Image.asset(source, width: 300, height: 300),
           ],
         ),
       ),
